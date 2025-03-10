d2f747a589437243e9b628d59b341701c0156843
一听曲就困 <zhangyuleyu@outlook.com>
2025-03-10 20:15:28 +0800

    无

--- Core/Src/KEY.c
+++ Core/Src/KEY.c
@@ -16,37 +16,38 @@ extern uint32_t led;
  * key4 暗
  * key5 确定
  */
-void key_control(){
-	if(HAL_GPIO_ReadPin(GPIOB, GPIO_PIN_12)==RESET)//key1 开关
-	{
+void key_control() {
+	if (HAL_GPIO_ReadPin(GPIOB, GPIO_PIN_12) == RESET) //key1 开关
+			{
 		HAL_Delay(20);
-		while(HAL_GPIO_ReadPin(GPIOB, GPIO_PIN_12)==RESET);
-		hal_ledpwm(0);
-
+		if (HAL_GPIO_ReadPin(GPIOB, GPIO_PIN_12) == RESET) {
+			while (HAL_GPIO_ReadPin(GPIOB, GPIO_PIN_12) == RESET);
+			hal_ledpwm(0);
+		}
 	}
-	else if(HAL_GPIO_ReadPin(GPIOB, GPIO_PIN_13)==RESET)//key2 模式切换
-	{
-		HAL_Delay(20);
-		while(HAL_GPIO_ReadPin(GPIOB, GPIO_PIN_13)==RESET);
-
-	}
-	else if(HAL_GPIO_ReadPin(GPIOB, GPIO_PIN_14)==RESET)//key3 亮
-	{
-		HAL_Delay(20);
-		while(HAL_GPIO_ReadPin(GPIOB, GPIO_PIN_14)==RESET);
-		//led+=10;
-	}
-	else if(HAL_GPIO_ReadPin(GPIOB, GPIO_PIN_15)==RESET)//key4 暗
-	{
-		HAL_Delay(20);
-		while(HAL_GPIO_ReadPin(GPIOB, GPIO_PIN_15)==RESET);
-		//led-=10;
-	}
-	else if(HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_11)==RESET)//key5 确定
-	{
-		HAL_Delay(20);
-		while(HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_11)==RESET);
-
-	}
+//	else if(HAL_GPIO_ReadPin(GPIOB, GPIO_PIN_13)==RESET)//key2 模式切换
+//	{
+//		HAL_Delay(20);
+//		while(HAL_GPIO_ReadPin(GPIOB, GPIO_PIN_13)==RESET);
+//
+//	}
+//	else if(HAL_GPIO_ReadPin(GPIOB, GPIO_PIN_14)==RESET)//key3 亮
+//	{
+//		HAL_Delay(20);
+//		while(HAL_GPIO_ReadPin(GPIOB, GPIO_PIN_14)==RESET);
+//		//led+=10;
+//	}
+//	else if(HAL_GPIO_ReadPin(GPIOB, GPIO_PIN_15)==RESET)//key4 暗
+//	{
+//		HAL_Delay(20);
+//		while(HAL_GPIO_ReadPin(GPIOB, GPIO_PIN_15)==RESET);
+//		//led-=10;
+//	}
+//	else if(HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_11)==RESET)//key5 确定
+//	{
+//		HAL_Delay(20);
+//		while(HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_11)==RESET);
+//
+//	}
 
 }
